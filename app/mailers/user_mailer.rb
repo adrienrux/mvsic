910640feb79b36fd4aa1e677d8c14dc9047e0839
James Zhang <james@wistia.com>
2014-08-03 17:44:09 -0400

    Change email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: 'mvsicio@gmail.com'
 
   def schedule_email(email, schedule)
+    @schedule = schedule
     @festival = schedule.festival
-    @url  = "http://mvsic.io/schedules/#{schedule.hashed_id}"
     @email = email
     subject = "Here's your Schedule for #{schedule.festival.name}!"
     puts 'Hello'
