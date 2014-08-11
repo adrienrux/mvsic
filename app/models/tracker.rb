47ae25fc94895c06c214077c312e1a99f198adeb
James Zhang <james@wistia.com>
2014-08-10 22:20:34 -0400

    Update seeds

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -4,6 +4,10 @@ class Tracker < ActiveRecord::Base
     update_attribute :count, read_attribute(:count) + 1
   end
 
+  def instance
+    instance_class.constantize.find(instance_id)
+  end
+
   def self.for(obj, subject)
     Tracker.where(instance_class: obj.class.to_s, instance_id: obj.id, subject: subject).first
   end
