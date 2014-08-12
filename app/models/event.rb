f2c824f131d9085a477e04a61fa6505a8b13eb3c
James Zhang <james@wistia.com>
2014-08-12 00:41:40 -0400

    Validate presence of artist and festival for event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :festival
   belongs_to :artist
   belongs_to :venue
-  validates :artist_id, :festival_id, presence: true
+  validates :artist, presence: true
+  validates :festival, presence: true
   has_many :schedule_events, dependent: :destroy
   has_many :schedules, through: :schedule_events
 end
